d77a32604560b63ecb84b12d1a631a7d10106e47
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-07-10 21:21:38 -0300

    feat - controller de Barbeiro e Usuario

---
+++ src/main/java/github/MaxBr221/controller/BarbeiroController.java
@@ -0,0 +1,53 @@
+package github.MaxBr221.controller;
+
+import github.MaxBr221.dtos.barbeiro.BarbeiroRequestDTO;
+import github.MaxBr221.dtos.barbeiro.BarbeiroResponseDTO;
+import github.MaxBr221.service.BarbeiroService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/barbeiro")
+@Slf4j
+@RequiredArgsConstructor
+public class BarbeiroController {
+    private BarbeiroService barbeiroService;
+
+    @PostMapping
+    public ResponseEntity<BarbeiroResponseDTO> create(BarbeiroRequestDTO barbeiroRequestDTO){
+        BarbeiroResponseDTO barbeiro = barbeiroService.create(barbeiroRequestDTO);
+        log.info("Salvando barbeiro {}", barbeiro.nome());
+        return ResponseEntity.status(HttpStatus.CREATED).body(barbeiro);
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> delete(Long id){
+        barbeiroService.delete(id);
+        log.info("Barbeiro apagado com sucesso!");
+        return ResponseEntity.noContent().build();
+
+    }
+    @PutMapping
+    public ResponseEntity<BarbeiroResponseDTO> update(Long id, BarbeiroRequestDTO barbeiroRequestDTO){
+        BarbeiroResponseDTO barbeiroUpdate = barbeiroService.update(id, barbeiroRequestDTO);
+        log.info("Barbeiro de id {} editado com sucesso!", barbeiroUpdate.id());
+        return ResponseEntity.ok(barbeiroUpdate);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<BarbeiroResponseDTO> findById(Long id){
+        BarbeiroResponseDTO barbeiroBuscado = barbeiroService.findById(id);
+        log.info("Barbeiro {} buscado com sucesso!", barbeiroBuscado.nome());
+        return ResponseEntity.ok(barbeiroBuscado);
+    }
+    @GetMapping
+    public ResponseEntity<List<BarbeiroResponseDTO>> findAll(){
+        List<BarbeiroResponseDTO> listaBarbeiro = barbeiroService.findAll();
+        log.info("Buscando todos os barbeiros!");
+        return ResponseEntity.ok(listaBarbeiro);
+
+    }
+}
--- src/main/java/github/MaxBr221/controller/UsuarioController.java
+++ src/main/java/github/MaxBr221/controller/UsuarioController.java
@@ -5,10 +5,11 @@ import github.MaxBr221.dtos.usuario.UsuarioResquestDTO;
 import github.MaxBr221.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +19,33 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public UsuarioResponseDTO create(@RequestBody UsuarioResquestDTO usuarioResquestDTO){
-
+    public ResponseEntity<UsuarioResponseDTO> create(@RequestBody UsuarioResquestDTO usuarioResquestDTO){
+        UsuarioResponseDTO usuario = usuarioService.create(usuarioResquestDTO);
+        log.info("Salvando usuário {}!", usuario.nome());
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> delete(Long id){
+        usuarioService.delete(id);
+        log.info("Deletando usuário!");
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping
+    public ResponseEntity<UsuarioResponseDTO> update(Long id, UsuarioResquestDTO usuarioResquestDTO){
+        UsuarioResponseDTO usuarioUpdate = usuarioService.update(id, usuarioResquestDTO);
+        log.info("Atualizando usuário do id {}", usuarioUpdate.id());
+        return ResponseEntity.ok(usuarioUpdate);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> findById(Long id){
+        UsuarioResponseDTO usuarioBuscado = usuarioService.findById(id);
+        log.info("Usuário buscado {} buscado com sucesso!", usuarioBuscado.nome());
+        return ResponseEntity.ok(usuarioBuscado);
+    }
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> findAll(){
+        List<UsuarioResponseDTO> listaUsuario = usuarioService.findAll();
+        log.info("Listando todos os usuários!");
+        return ResponseEntity.ok(listaUsuario);
     }
 }
